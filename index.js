33a78b5c09bea2f383d45dac96d4f8181b0fc48e
teo-crth <teoconrath@gmail.com>
2024-12-08 11:49:33 +0100

    generate readme

---
+++ index.js
@@ -0,0 +1,70 @@
+import { promises as fs } from 'fs';
+import readme from './readme';
+
+const msInOneDay = 1000 * 60 * 60 * 24;
+
+const today = new Date();
+
+function generateNewREADME() {
+  const readmeRow = readme.split('\n');
+
+  function updateIdentifier(identifier, replaceText) {
+    const identifierIndex = findIdentifierIndex(readmeRow, identifier);
+    if (!readmeRow[identifierIndex]) return;
+    readmeRow[identifierIndex] = readmeRow[identifierIndex].replace(
+      `<#${identifier}>`,
+      replaceText
+    );
+  }
+
+  const identifierToUpdate = {
+    day_before_new_years: getDBNWSentence(),
+    today_date: getTodayDate(),
+    signing: getSigning(),
+  };
+
+  Object.entries(identifierToUpdate).forEach(([key, value]) => {
+    updateIdentifier(key, value);
+  });
+
+  return readmeRow.join('\n');
+}
+
+const emojiOfTheDay = {
+  1: "👋",
+  2: '📖',
+  3: '🌱',
+  4: '🐻',
+  5: '🐧'
+};
+
+function getSigning() {
+  const emoji = emojiOfTheDay[today.getDay() + 1];
+  return `Emoji of the Day ${emoji}`;
+}
+
+function getTodayDate() {
+  return today.toDateString();
+}
+
+function getDBNWSentence() {
+  const nextYear = today.getFullYear() + 1;
+  const nextYearDate = new Date(String(nextYear));
+
+  const timeUntilNewYear = nextYearDate.getTime() - today.getTime();
+  const dayUntilNewYear = Math.round(timeUntilNewYear / msInOneDay);
+
+  return `**${dayUntilNewYear} day before ${nextYear} ⏱**`;
+}
+
+const findIdentifierIndex = (rows, identifier) =>
+  rows.findIndex((r) => Boolean(r.match(new RegExp(`<#${identifier}>`, 'i'))));
+
+const updateREADMEFile = (text) => fs.writeFile('./README.md', text);
+
+function main() {
+  const newREADME = generateNewREADME();
+  console.log(newREADME);
+  updateREADMEFile(newREADME);
+}
+main();
---
+++ readme.js
@@ -0,0 +1,52 @@
+
+const readme = `# 〰〰〰 Développeur Fullstack 〰〰〰
+
+## Skills
+
+- Typescript
+- Javascript
+- Git / Github
+- Jest / Chai
+- vite
+
+
+### Front end
+
+- React
+- Vue.js
+- HTML / CSS
+- Sass / Scss
+
+### Back end
+
+  - Node.js
+  - Express
+  - SQL
+  - PostgreSQL / MongoDB
+  - Templating EJS
+  - API REST
+  
+  ### CMS
+  
+  - Wordpress
+  - Prestaphop
+  - Drupal
+  
+  ### Tagging / Tracking
+  - Google Tag Manager
+  - Google Analytics / Google Ads
+  - Affiliation
+  
+  ## Interests
+  
+  ## 🤺 📖 💻 🏍 🪴
+
+  ## 
+  Last update on <#today_date>
+
+  <#day_before_new_years> days before new years
+
+  <#signing>
+  `;
+
+module.exports = readme;
